ddad838bcc67f5928fc3fbe057443224ca5764ca
Matthew G <matthewgard1@yahoo.com>
2015-11-13 16:50:09 -0700

    paths

---
+++ Assets/Pixelplacement/iTweenPath/tester.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tester : MonoBehaviour
+{
+	void Start(){
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("zigzag"), "time", 30));
+	}
+
+}
