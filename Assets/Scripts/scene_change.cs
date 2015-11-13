cb075a231c4c6d6434e43424c5676cc01abcfa6d
Cam H <ilikecam8D@icloud.com>
2015-11-13 09:38:33 -0700

    load new scene script

---
+++ Assets/Scripts/scene_change.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scene_change : MonoBehaviour {
+
+
+
+	//set variables here
+
+	//___example__
+	//public type variable;
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}//end update
+
+	void OnMouseDown(){
+
+		LoadScene()
+	}//end on click
+
+
+	void LoadScene (){
+		Application.LoadLevel ("bowling_scene2");
+	}//end load scene
+}
