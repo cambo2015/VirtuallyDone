4b8212e842d1fc497800e931d0f8f5ba59006b5d
Brad Guasp <Brad@ip-144-38-136-233.dixie.edu>
2015-11-13 19:19:07 -0700

    adding the ball/collision.

---
+++ Assets/Scripts/destroy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroy : MonoBehaviour 
+{
+
+	public Rigidbody Ball;
+
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Ball")) 
+		{
+			Destroy (Ball);
+		}
+	}
+}
