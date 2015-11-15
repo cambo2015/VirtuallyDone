4348ffc3caa5ffebfcf8e15117249260f6410ee0
Cam H <ilikecam8D@icloud.com>
2015-11-14 21:40:22 -0700

    animated china swamp monster

---
+++ Assets/Scripts/start_moving.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start_moving : MonoBehaviour {
+	Animator anim;//state animator variable
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator> (); //set animation variable
+		//anim.SetBool ("run",true);
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("move_monster"), "time", 20,"orienttopath",true,"loop","pingpong","easeType","linear","axis","y"));
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("run", true);//run variable
+	}
+}
